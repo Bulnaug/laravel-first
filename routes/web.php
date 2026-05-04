d8ff1df4b3efefdfb1d7c9655a978a9e6f3d0981
Bulnaug <basalay.1997@mail.ru>
2026-05-04 23:37:14 +0200

    feat: add auth with Breeze and fix layout issues

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return view('hello');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    
+    Route::get('/contacts', [ContactController::class, 'index']);
+    Route::get('/contacts/create', [ContactController::class, 'create']);
+    Route::post('/contacts', [ContactController::class, 'store']);
+    Route::get('/contacts/{id}', [ContactController::class, 'show']);
+    Route::get('/contacts/{id}/edit', [ContactController::class, 'edit']);
+    Route::put('/contacts/{id}', [ContactController::class, 'update']);
+    Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
 });
 
-use App\Http\Controllers\ContactController;
-
-Route::get('/contacts', [ContactController::class, 'index']);
-Route::get('/contacts/create', [ContactController::class, 'create']);
-Route::post('/contacts', [ContactController::class, 'store']);
-Route::get('/contacts/{id}/edit', [ContactController::class, 'edit']);
-Route::put('/contacts/{id}', [ContactController::class, 'update']);
-Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
-Route::get('/contacts/{id}', [ContactController::class, 'show']);
+require __DIR__.'/auth.php';
